404a369897e9fd77bb6fe4749e9eb6a6833a30a6
VolodymyrVelch <velch83@gmail.com>
2022-06-06 22:19:18 +0300

    function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-"use strict";
+'use strict';
 
-const messege = "JS";
+const messege = 'JS';
 console.log(messege);
 
+const userName = 'voffka';
 const x = 10;
-const y = 5;
+const y = 6;
 
 console.log(x + y);
 
 console.log(x * y);
+
+// alert('qwqe');
+
+// Приведеня до степеня:
+let base = prompt('Введи число');
+base = Number(base);
+console.log('Число', base);
+
+let power = prompt('Введи степінь');
+power = Number(power);
+console.log('Cтепінь', power);
+
+let result = base ** power;
+console.log('Результат', result);
