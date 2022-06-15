9fd38b0678dd312a26c5cf234d60c213bc0f00c3
VolodymyrVelch <velch83@gmail.com>
2022-06-15 19:33:12 +0300

    autocheck_9

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const messege = 'JS';
-console.log(messege);
+// const messege = 'JS';
+// console.log(messege);
 
-const userName = 'voffka';
-const x = 10;
-const y = 6;
+// const userName = 'voffka';
+// const x = 10;
+// const y = 6;
 
-console.log(x + y);
+// console.log(x + y);
 
-console.log(x * y);
+// console.log(x * y);
 
 // alert('qwqe');
 
@@ -24,3 +24,13 @@ console.log(x * y);
 
 // let result = base ** power;
 // console.log('Результат', result);
+
+const numbers = [67, 54, 9, 200, 4, 32, 148, 11, 68];
+let biggestNum = numbers[0];
+
+for (const number of numbers) {
+  if (biggestNum < number) {
+    biggestNum = number;
+  }
+}
+console.log(biggestNum);
--- js/script2.js
+++ js/script2.js
@@ -13,20 +13,80 @@
 // якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, запиши у змінну message рядок "Insufficient funds!";
 // в іншому випадку, відніми суму покупки з рахунку клієнта і запиши у змінну message повідомлення: "You ordered <число> droids, you have <число> credits left".
 
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-  const totalPrice = pricePerDroid * orderedQuantity;
-  if (customerCredits >= totalPrice) {
-    message = `You ordered ${orderedQuantity} droids, you have ${
-      customerCredits - totalPrice
-    } credits left`;
-  } else {
-    message = 'Insufficient funds!';
-  }
-  // Change code above this line
-  return message;
-}
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+//   if (customerCredits >= totalPrice) {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   } else {
+//     message = 'Insufficient funds!';
+//   }
+//   // Change code above this line
+//   return message;
+// }
 
-console.log(makeTransaction(1200, 4, 8000));
-console.log(makeTransaction(1200, 4, 4000));
+// console.log(makeTransaction(1200, 4, 8000));
+// console.log(makeTransaction(1200, 4, 4000));
+
+// 2=====================
+// Функція checkPassword отримує пароль користувача у
+// параметр password, перевіряє його на збіг з паролем
+//адміністратора у змінній ADMIN_PASSWORD і повертає повідомлення про
+//результат порівняння.
+
+// Виконай рефакторинг коду функції checkPassword, використовуючи патерн
+// «раннє повернення»:
+
+// видали змінну message
+// видали else
+// код повинен працювати так само, як і до оптимізації
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+
+//   return 'Access denied, wrong password!';
+
+//   // Change code above this line
+// }
+// console.log(checkPassword('password'));
+// console.log(checkPassword('jqueryismyjam'));
+
+// 3=================
+// function checkStorage(available, ordered) {
+//   // Change code below this line
+
+//   if (ordered === 0) {
+//     return 'Your order is empty!';
+//   }
+//   if (ordered > available) {
+//     return 'Your order is too large, not enough goods in stock!';
+//   }
+//   return 'The order is accepted, our manager will contact you';
+
+//   // Change code above this line
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(5, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+// 4==================
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// // Change code below this line
+// console.log('firstElement', fruits[0]);
+// console.log('secondElement', fruits[1]);
+// const lastElementIndex = fruits.length - 1;
+// console.log('lastElement', fruits[lastElementIndex]);
+
+// 9 ======================
