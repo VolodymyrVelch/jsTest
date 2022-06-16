f066e242874f3cea42e526eb837c5814c027c1fd
VolodymyrVelch <velch83@gmail.com>
2022-06-16 10:42:45 +0300

    tast11_branch_pc

--- js/script2.js
+++ js/script2.js
@@ -89,4 +89,27 @@
 // const lastElementIndex = fruits.length - 1;
 // console.log('lastElement', fruits[lastElementIndex]);
 
-// 9 ======================
+// 11 ======================
+// Сервісу гравірування прикрас потрібна функція, яка б автоматично рахувала ціну 
+// гравірування, залежно від кількості слів і ціни за слово.
+
+// Оголошена функція calculateEngravingPrice(message, pricePerWord).
+//  Ця функція приймає рядок, що складається зі слів, розділених лише пробілами 
+//  (параметр message) та ціну гравірування одного слова (параметр pricePerWord).
+
+// Напиши тіло функції, щоб вона повертала загальну вартість гравірування 
+// усіх слів в рядку.
+
+
+function calculateEngravingPrice(message, pricePerWord) {
+    // Change code below this line
+   const arraySeparate = message.split(' ');
+   const arrayLenth= [arraySeparate.length];
+
+   const messageLenth = Number(arrayLenth);
+ 
+   return messageLenth * pricePerWord;
+    // Change code above this line
+ }
+
+ console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
