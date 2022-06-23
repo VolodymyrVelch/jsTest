6a905eb62e308f66c668b4ff0c0e34b197fde62e
VolodymyrVelch <velch83@gmail.com>
2022-06-23 22:54:01 +0300

    mod3_24task

--- js/script3.js
+++ js/script3.js
@@ -426,3 +426,44 @@
 // console.log(meanTemperature);
 
 // 22=========================================
+
+// У прогнозі максимальних температур також може бути необов'язкова
+// властивість icon - іконка погоди.Заміни оголошення змінних yesterday,
+//     today, tomorrow і icon однією операцією деструктуризації властивостей
+//     об'єкта highTemperatures. Задай значення за замовчуванням для icon -
+//     рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+// // const yesterday = highTemperatures.yesterday;
+// // const today = highTemperatures.today;
+// // const tomorrow = highTemperatures.tomorrow;
+// // const icon = highTemperatures.icon;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(icon);
+// Оголошена змінна highTemperatures
+// Значення змінної highTemperatures - це об'єкт
+// Оголошена змінна highTemperatures
+// Значення змінної highTemperatures - це об'єкт
+// Оголошена змінна yesterday за допомогою деструктуризації
+// Значення змінної yesterday - це число 28
+// Оголошена змінна today за допомогою деструктуризації
+// Значення змінної today - це число 26
+// Оголошена змінна tomorrow за допомогою деструктуризації
+// Значення змінної tomorrow - це число 33
+// Оголошена змінна icon за допомогою деструктуризації
+// Значення змінної icon - це рядок "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+// Використовується деструктуризація об'єкта
+
+// 24==========================================
