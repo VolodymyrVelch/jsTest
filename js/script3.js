971ae09e92691ffb6b5acbf9a332879b54ab0d0e
VolodymyrVelch <velch83@gmail.com>
2022-06-26 18:50:56 +0300

    4mod

--- js/script3.js
+++ js/script3.js
@@ -748,51 +748,40 @@ const atTheOldToad = {
   },
   addPotion(newPotion) {
     for (const potion of this.potions) {
-      //   console.log(potion.name);
       if (potion.name === newPotion.name) {
         return `Error! Potion ${newPotion.name} is already in your inventory!`;
       }
     }
     this.potions.push(newPotion);
   },
-
   removePotion(potionName) {
-    for (let i = 0; i <= this.potions.length; i += 1) {
-      const potName = this.potions[i];
-      if (potName.name === potionName) {
-        // let indexPot = indexOf(potName);
-        this.potions.splice(i, 1);
+    const { potions } = this;
+    for (let i = 0; i < potions.length; i += 1) {
+      const { name } = potions[i];
+      if (potionName === name) {
+        potions.splice(i, 1);
+        // console.table(this);
+        return;
       }
     }
     return `Potion ${potionName} is not in inventory!`;
   },
-  //   for (let namePot of name) {
-  //   const namePot = this.potions.name;
-
-  // if (namePot[0] === potionName) {
-  //   potions.splice(i, 1);
-  // }
-
-  //     return `Potion ${potionName} is not in inventory!`;
-  //   },
-  //   updatePotionName(oldName, newName) {
-  //     const potionIndex = this.potions.indexOf(oldName);
-
-  //     if (potionIndex === -1) {
-  //       return `Potion ${oldName} is not in inventory!`;
-  //     }
-
-  //     this.potions.splice(potionIndex, 1, newName);
-  //   },
+  updatePotionName(oldName, newName) {
+    for (const nameIn of this.potions) {
+      if (nameIn.name === oldName) {
+        nameIn.name = newName;
+        return;
+      }
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
   // Change code above this line
-  //   }
 };
-
+console.log(atTheOldToad.removePotion('Dragon breath'));
 // console.log(atTheOldToad.getPotions());
 // console.log(atTheOldToad.addPotion({ name: 'Invisibility', price: 620 }));
 // console.log(atTheOldToad.addPotion({ name: 'Power potion', price: 270 }));
 // console.log(atTheOldToad.addPotion({ name: 'Dragon breath', price: 700 }));
-console.log(atTheOldToad.removePotion('Dragon breath'));
 // console.log();
 
 // Виклик методу atTheOldToad.getPotions() для вихідного об'єкта повертає [ { name: "Speed potion", price: 460 }, { name: "Dragon breath", price: 780 }, { name: "Stone skin", price: 520 } ]
--- js/script4.js
+++ js/script4.js
@@ -1,20 +1,63 @@
-const atTheOldToad = {
-  potions: [
-    { name: 'Speed potion', price: 460 },
-    { name: 'Dragon breath', price: 780 },
-    { name: 'Stone skin', price: 520 },
-  ],
-  // Change code below this line
+// // Колбек-функція------------------------------------
 
-  removePotion(potionName) {
-    for (let i = 0; i <= this.potions.length; i += 1) {
-      if (potionName === this.potions[i].name) {
-        this.potions.splice(i, 1);
-        // return;
-      }
-    }
-    return `Potion ${potionName} is not in inventory!`;
-  },
+// function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// }
+
+// // Функція вищого порядку
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest('Манго', greet);
+
+// Інлайн функція ------------------------------------
+
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// // Передаємо інлайн функцію greet у якості колбека
+// registerGuest('Манго', function greet(name) {
+//   console.log(`Ласкаво просимо ${name}.`);
+// });
+
+// // Передаємо інлайн функцію notify у якості колбека
+// registerGuest('Полі', function notify(name) {
+//   console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+// });
+// ----------------------
+// function printValue(value) {
+//   console.log(value);
+// }
+
+// function prettyPrint(value) {
+//   console.log('Logging value: ', value);
+// }
+
+// function repeat(n, action) {
+//   for (let i = 0; i < n; i += 1) {
+//     action(i);
+//   }
+// }
+
+// // Передаємо printValue як callback-функцію
+// repeat(3, printValue);
+// // 0
+// // 1
+// // 2
+
+// // Передаємо prettyPrint як callback-функцію
+// repeat(3, prettyPrint);
+// // Logging value: 0
+// // Logging value: 1
+// // Logging value: 2
+
+// =>=========================
+const add = (...args) => {
+  console.log(args);
 };
 
-console.log(atTheOldToad.removePotion('Dragon breath'));
+add(1, 2, 3, 6); // [1, 2, 3]
