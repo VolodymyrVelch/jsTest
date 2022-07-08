187cf945db5ef2d04f4c3227798a7b0d1ebd8caa
VolodymyrVelch <velch83@gmail.com>
2022-07-08 21:07:01 +0300

    5mod_taskDone

--- js/script5.js
+++ js/script5.js
@@ -413,3 +413,240 @@
 // console.log(builder.getValue()); // "=^.^="
 
 // 15=========================================
+
+// Виконай рефакторинг класу Car.Додай публічну статичну властивість MAX_PRICE
+//  зі значенням 50000 - максимально допустима ціна автомобіля.
+
+// Додай сетеру price перевірку значення параметра newPrice, що передається.
+// Якщо воно більше за MAX_PRICE, сеттер нічого не робить, а якщо менше або
+// дорівнює, то перезаписує ціну автомобіля.
+
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+//   #price;
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// 17=================================
+// Додай класу Car публічний статичний метод checkPrice(price), що приймає ціну
+// автомобіля.Метод повинен порівняти значення параметра price і приватної
+//  статичного властивості MAX_PRICE.
+
+// Якщо ціна автомобіля перевищує максимальну, метод повинен повернути рядок
+// "Error! Price exceeds the maximum".
+// В іншому випадку метод повинен повернути рядок "Success! Price is within
+// acceptable limits".
+// Під оголошенням класу ми додали ініціалізацію екземпляра і виклики методів,
+//     щоб показати, як буде використовуватися метод checkPrice(price).
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     return price <= Car.#MAX_PRICE
+//       ? 'Success! Price is within acceptable limits'
+//       : 'Error! Price exceeds the maximum';
+//   }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// 18=========================================
+
+// У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у
+// чорний список.
+
+// Оголоси клас Admin, який наслідує від класу User
+// Додай класу Admin публічну статичну властивість AccessLevel(рівень доступу),
+//     значення якої — це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' };
+// }
+
+// // Change code below this line
+// // Оголошений клас Admin
+// // Клас Admin наслідує від класу User
+// // Клас Admin містить публічну статичну властивість AccessLevel
+// console.log(Admin.AccessLevel.BASIC); // повертає рядок "basic"
+// console.log(Admin.AccessLevel.SUPERUSER); // повертає рядок "superuser"
+
+// 19==================================
+// Додай класу Admin метод constructor, який приймає один параметр -
+//     об'єкт налаштувань з двома властивостями email і accessLevel. Додай класу
+//      Admin публічну властивість accessLevel, значення якої буде передаватися
+//       під час виклику конструктора.
+
+// Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію
+// екземпляра під оголошенням класу.
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     Admin.accessLevel = accessLevel;
+//   }
+//   get accessLevel() {
+//     return Admin.accessLevel;
+//   }
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// // Оголошений клас Admin
+// // Клас Admin наслідує від класу User
+// // Клас Admin містить публічну статичну властивість AccessLevel
+// // Клас Admin містить метод constructor з параметром у вигляді об'єкта
+// // { email, accessLevel }
+// // В класі Admin в конструкторі для властивості email використовується звернення
+// // до конструктора батьківського класу
+// console.log(Admin.AccessLevel.BASIC); // повертає рядок "basic"
+// console.log(Admin.AccessLevel.SUPERUSER); // повертає рядок "superus
+
+// 20========================================
+// Додай класу Admin наступні властивості і методи.
+
+// Публічну властивість blacklistedEmails для зберігання чорного списку поштових
+// адрес користувачів.Значення за замовчуванням — це порожній масив.
+// Публічний метод blacklist(email) для додавання пошти у чорний список.
+//  Метод повинен додавати значення параметра email в масив, що зберігається
+//  у властивості blacklistedEmails.
+// Публічний метод isBlacklisted(email) для перевірки пошти у чорному списку.
+//  Метод повинен перевіряти наявність значення параметра email в масиві, що
+//  зберігається у властивості blacklistedEmails, і повертати true або false.
+// Після оголошення класу ми додали ініціалізацію екземпляра і виклики методів
+//  у тій послідовності, в якій твій код перевірятимуть тести.Будь ласка,
+//     нічого там не змінюй.
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//     this.blacklistedEmails = [];
+//   }
+
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+//   isBlacklisted(email) {
+//     return this.blacklistedEmails.includes(email);
+//   }
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist('poly@mail.com');
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted('mango@mail.com')); // false
+// console.log(mango.isBlacklisted('poly@mail.com')); // true
+
+// // Оголошений клас Admin
+// // Клас Admin наслідує від класу User
+// // Клас Admin містить публічну властивість blacklistedEmails
+// // Клас Admin містить публічний метод blacklist
+// // Клас Admin містить публічний метод isBlacklisted
+// console.log(mango.blacklist('poly@mail.com')); // значення властивості blacklistedEmails - це масив ["poly@mail.com"]
+// console.log(mango.isBlacklisted('mango@mail.com')); // повертає false
+// console.log(mango.isBlacklisted('poly@mail.com')); // повертає true
---
+++ js/scriptCodeWar.js
@@ -0,0 +1,18 @@
+// Квадрат
+// function squareSum(numbers) {
+//   let square = 0;
+//   for (const number of numbers) {
+//     square += Math.pow(number, 2);
+//   }
+//   return square;
+// }
+
+// function squareSum(numbers) {
+//   return numbers.reduce((val, number) => (val += Math.pow(number, 2)), 0);
+// }
+// const squareSum = numbers =>
+//   numbers.reduce((val, number) => (val += Math.pow(number, 2)), 0);
+
+// console.log(squareSum([1, 2])); //, 5);
+// console.log(squareSum([0, 3, 4, 5])); //, 50);
+// console.log(squareSum([])); //, 0);
